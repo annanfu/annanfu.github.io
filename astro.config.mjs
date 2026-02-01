30bc38c4bb95b5c21e25e9d4d7b2bc00020af33a
annanfu <fu.anna@northeastern.edu>
2026-01-31 22:43:09 -0800

    Update pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://annanfu.github.io",
-  base: "/", 
+  base: "/",
+  devToolbar: { enabled: false },
 });
